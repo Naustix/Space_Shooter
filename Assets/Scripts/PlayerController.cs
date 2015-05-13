406b35bdf6fe6550e22ba629641f0ce4509eea04
Christopher Austin Bryant <naustix.bryant@gmail.com>
2015-05-12 19:28:01 -0500

    Space_Shooter
    UPDATED for Unity 5.0.1f1 - From the Space Shooter Tutorial for Unity 4

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	private GameObject bolt; // Had to do this, different from tutorial
+	public GameObject shot;
+	public GameObject shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetButton ("Jump") && Time.time > nextFire)
+		{
+			bolt = Instantiate(shot) as GameObject; // If I don't do this, the shots get destroyed by boundary and player cannot shoot anymore.
+			nextFire = Time.time + fireRate;
+//			GameObject clone = 
+			bolt.transform.position = shotSpawn.transform.position; // This works fine, a good solution.
+
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3
+		(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Space_Shooter
+UPDATED for Unity 5.0.1.f1 - From the Space Shooter Tutorial for Unity 4
